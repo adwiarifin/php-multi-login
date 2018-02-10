cb2477844db69627e27935007151a6304a28b486
Adwi Arifin <adwiarifin@gmail.com>
2018-02-10 13:32:18 +0700

    add bootstrap and font-awesome in index.php

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
-<?php 
-require 'vendor/autoload.php';
-require 'opauth.conf.php';
-
-$auth = new Opauth($config);
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Multi Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-9 col-md-offset-1">
+	            <ul>
+	                <li>
+	                    <a href="#" class="btn btn-block btn-lg btn-social btn-facebook social-button">
+	                        <span class="fa fa-facebook"></span> login with Facebook
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="#" class="btn btn-block btn-lg btn-social btn-google social-button">
+	                        <span class="fa fa-google"></span> login with Google
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="#" class="btn btn-block btn-lg btn-social btn-twitter social-button">
+	                        <span class="fa fa-twitter"></span> login with Twitter
+	                    </a>
+	                </li>
+	            </ul>
+	        </div>
+	    </div>
+	</div>
+</body>
+</html>
